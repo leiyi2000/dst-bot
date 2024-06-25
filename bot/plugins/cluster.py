efe32553d6103f6ae0338bb7f53c0e7a84db124e
ylei <2457738122@qq.com>
2024-06-26 02:42:47 +0800

    fix 存档文件名命名错误

--- bot/plugins/cluster.py
+++ bot/plugins/cluster.py
@@ -10,7 +10,7 @@ router = CommandRouter()
 
 @router.command("蜘蛛备份", inject_event=False)
 async def backup():
-    name = f"蜘蛛-{datetime.now().strftime('%Y%m%d%H%M')}"
+    name = f"蜘蛛-{datetime.now().strftime('%Y%m%d%H%M')}.zip"
     file = await napcat.download_file("http://113.31.186.221:8000/cluster/download/3")
     return Message(
         type="file",
