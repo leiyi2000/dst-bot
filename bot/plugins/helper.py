e8760fd251237c7a1dc46f74981bfeb248a133f6
ylei <2457738122@qq.com>
2024-07-27 17:35:51 +0800

    优化 ls指令返回格式

--- bot/plugins/helper.py
+++ bot/plugins/helper.py
@@ -10,17 +10,17 @@ router = CommandRouter()
 
 @router.command("ls", inject_event=False)
 async def ls():
-    room_short = ""
+    cluster_id = ""
     reply_message = "本群服务器如下: \n\n"
     async with httpx.AsyncClient() as client:
         url = f"{WENDY_API}/deploy?status=running"
         response = await client.get(url)
         for cluster in response.json():
-            reply_message += (
-                f'{cluster["id"]}. {cluster["content"]["ini"]["cluster_name"]}\n'
-            )
-    if room_short:
-        reply_message += f"\n发送备份指令即可备份存至群文件如: 备份{room_short}\n"
+            cluster_id = cluster["id"]
+            cluster_name = cluster["content"]["ini"]["cluster_name"]
+            reply_message += f"{cluster_id}. {cluster_name}\n"
+    if cluster_id:
+        reply_message += f"\n发送备份指令即可备份存至群文件如: 备份{cluster_id}\n"
         reply_message += "注意: 备份存档会引起游戏卡顿"
         return [NodeMessage(content=reply_message)]
     else:
